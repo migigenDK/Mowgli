4add7a7ce0789a6e23f9ccb93df96e4e77dac6d9
Pepeuch <pepeuch@gmail.com>
2025-05-12 20:08:01 +0200

    mise a jour des fonctions dans blademotor, drivemotor et panel suppresion des message d'avertissements usb dans usbd_conf 	modified:   stm32/ros_usbnode/.vscode/settings.json 	modified:   stm32/ros_usbnode/include/usbd_conf.h 	modified:   stm32/ros_usbnode/src/blademotor.c 	modified:   stm32/ros_usbnode/src/drivemotor.c 	modified:   stm32/ros_usbnode/src/panel.c

--- stm32/ros_usbnode/src/drivemotor.c
+++ stm32/ros_usbnode/src/drivemotor.c
@@ -145,7 +145,7 @@ void DRIVEMOTOR_Init(void)
 
     // RX
     GPIO_InitStruct.Pin = DRIVEMOTORS_USART_RX_PIN;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_INPUT;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
 #if BOARD_YARDFORCE500_VARIANT_B
